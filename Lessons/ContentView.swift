4f1b4fccff86491c1ed72bf95edff87f07d7fd0c
Vladyslav R <160039489+55kt@users.noreply.github.com>
2025-05-16 17:35:43 +0300

    Initial Commit

---
+++ Lessons/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lessons
+//
+//  Created by Vlad on 16/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lessons/LessonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LessonsApp.swift
+//  Lessons
+//
+//  Created by Vlad on 16/5/25.
+//
+
+import SwiftUI
+
+@main
+struct LessonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
