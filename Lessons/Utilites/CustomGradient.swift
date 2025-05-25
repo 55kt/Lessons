3fffbb04f00038d14dfa8df63856d0b23677d35e
Vladyslav R <160039489+55kt@users.noreply.github.com>
2025-05-25 21:30:32 +0300

    gradient2

--- Lessons/Utilites/CustomGradient.swift
+++ Lessons/Utilites/CustomGradient.swift
@@ -11,3 +11,8 @@ public func customGradient(_ color1: Color, _ color2: Color) -> some View {
     LinearGradient(colors: [color1, color2], startPoint: .topLeading, endPoint: .bottomTrailing)
         .ignoresSafeArea(.all)
 }
+
+public func customGradient2(_ color1: Color, _ color2: Color) -> some View {
+    LinearGradient(colors: [color1, color2], startPoint: .topLeading, endPoint: .bottomTrailing)
+        .ignoresSafeArea(.all)
+}
